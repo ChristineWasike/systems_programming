3f894fbfe2e17a65925a5a6d5369ed83cd086cd6
ChristineWasike <christinewasike22@gmail.com>
2020-09-24 17:05:22 +0300

    Working on the 4th question

--- week_two/question_1/question_1.c
+++ week_two/question_1/question_1.c
@@ -1,7 +1,28 @@
+/* Write a C program that capitalizes the first letter 
+of every word in a sentence . Use a sub-function to 
+capitalize the first letter of a word and call this 
+sub-function in the main() where you will display 
+the output sentence where the first letter of each 
+word is capitalized. [2 pts]*/
+
+// Break the string into an array of works
+
+//IN THE SUBFUNCTION(Capitalize Function)
+// Take in a word as an argument
+// 
+
 #include <stdio.h>
+
+const char* capitalize(){
+    return "Hello";
+}
 
 int main(){
     int a = 0;
-    printf("Something here %d", a);
+    printf("Something here %d \n", a);
+
+    printf("%s \n",capitalize());
     return 0;
 }
+
+
---
+++ week_two/question_3/question_3.c
@@ -0,0 +1,62 @@
+/*
+Let's code a game
+
+This program will  read a random number (like the program generates a random number)  
+and ask the user to guess it. The user has to guess the number that the program 
+generated. The program will give 10 attempts to guess the number, on each attempt 
+the program will inform that the entered number is less than or greater than the 
+random generated number so that the user can easily guess that particular number. 
+
+If the user guesses the number, you congratulate them and you tell them the number 
+of guesses they made .
+
+Eg : congratulations , you made 5 guesses.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(){
+    // Initialize random number
+    int randomNumber = rand() % 50;
+    // printf("The random number is: %d \n", randomNumber);
+    // Initialize variable to store the user's number
+    int userInput;
+
+    // Initializing the attempts made
+    int attempts = 0;
+    
+    do{
+        attempts += 1;
+        // Prompting user for a guess of the random number.
+        printf("Hello there! What number I'm thinking about? ");
+        scanf("%d", &userInput);
+        printf("You guesssed: %d \n", userInput);
+
+        int difference = randomNumber - userInput;
+        if (difference == 0){
+            if (attempts == 1){
+                printf("That's the right guess. You attempted this question once.\n");
+            } else{
+                printf("That's the right guess. You attempted this question %d times.\n", attempts);
+            }
+            
+            
+            break;
+        } else if( difference > 0){
+            printf("Your guess is less than my number. \n");
+
+        } else {
+            printf("Your guess is greater than my number. \n");
+        }
+        
+
+    } while(attempts < 10);
+
+    if (userInput != randomNumber){
+        printf("Sorry, you've used up all your tries.\n");
+    }
+    
+    
+}
